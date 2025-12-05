2e53e101e8d649ca1db9219e995f7de4dd494210
Bruno-spj <bruno_spj@hotmail.com>
2025-12-04 23:29:14 -0300

    adicionar interações ao DOM, criação de botão de Troca de tema de Light para Dark

--- Readme.md
+++ Readme.md
@@ -8,4 +8,6 @@
 - Versão em desenvolvimento - Dia 4: aprimora layout geral e define identidade visual inicial
 -  Versão em desenvolvimento - Dia 5: cria estrutura inicial da seção de projetos com cards básicos
 - Versão em desenvolvimento - Dia 6: criação de variáveis css e aplicação no projeto
-- Versão em desenvolvimento - dia 7: adiciona interações JS - form, ano
+- Versão em desenvolvimento - Dia 7: adiciona interações JS - form, ano
+- Versão em desenvolvimento - Dia 8: JavaScript Moderno: Manipulação do DOM na prática
+- Versão em desenvolvimento - Dia 9: adicionar interações ao DOM, criação de botão de Troca de tema de Light para Dark
--- js/index.js
+++ js/index.js
@@ -8,6 +8,19 @@ document.getElementById('contact-form').addEventListener('submit', function(even
   document.getElementById('contact-form').reset();
 });
 
+document.getElementById('btn-troca-tema').addEventListener('click', function() {
+  document.body.classList.toggle('dark-theme');
+
+  const imgBtn = document.getElementById('btn-troca-tema').querySelector('img');
+  if (document.body.classList.contains('dark-theme')) {
+    imgBtn.src = 'src/img/moon.png';
+    imgBtn.alt = 'Tema Escuro';
+  } else {
+    imgBtn.src = 'src/img/sun.png';
+    imgBtn.alt = 'Tema Claro';
+  }
+});
+
 const anoFooter = document.getElementById('ano-footer');
 const anoAtual = new Date().getFullYear();
 anoFooter.textContent = anoAtual;
